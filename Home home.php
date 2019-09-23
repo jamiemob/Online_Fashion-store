15ecb10ffdd83020d20017e2441a38c353f5796e
jamiemob <44671518+jamiemob@users.noreply.github.com>
2019-09-23 15:44:16 +0300

    Add files via upload

---
+++ Home home.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+      <head>
+        <title>Legacy Drip</title>
+      </head>
+<style>
+.p{
+	color:red;
+}
+.menu{
+	  width:100%;
+	  background:black;
+	  overflow:auto;
+   }
+ .menu ul{
+	  margin:0;
+	  padding:0;
+	  list-style:none;
+	  line-height:60px;
+  }
+ .menu ul li{
+	 float:left;
+	 margin-right:5px;
+ }
+ .menu ul li a{
+	  background:black;
+	  text-decoration:none;
+	  width:130px;
+	  display:block;
+	  text-align:center;
+	  color:#f2f2f2;
+	  font-size:17px;
+	  font-family:sans-serif;
+	  letter-spacing:0.5px;
+  }
+ .menu li a:hover{
+	 color:#fff;
+	 opacity:0.5;
+	 font-size:19px;	 
+ }
+ .menu ul li ul li{
+	 display:none;
+ }
+.menu ul li:hover ul li{
+	display:block;
+}
+.search-form{
+	margin-top:15px;
+	float:right;
+	margin-right:100px;
+}
+input [type=text]{
+	padding:7px;
+	border:none;
+	font-size:16px;
+	font-family:Arial;
+}
+button{
+	float:right;
+	background:orange;
+	color:white;
+	border-radius:0 5px 5px 0;
+	cursor:pointer;
+	position:relative;
+	font-family:sans-serif;
+	border:none;
+	font-size:16px;
+}
+</style>	  
+  <body background="Drip 1.jpg">
+    <h1><center><i>LEGACY PLACE STORES </i></center></h1>
+    <h2>HOME OF DRIP</h2>
+	<nav class ="menu">
+	<ul>
+	<li><a>Sign up/Log in</a>
+	    <ul>
+		  <li><a href="#Admin_login">Admin</a></li>
+	      <li><a href="#Vendor_login">Vendor</a></li>
+		  <li><a href="#Customer_login">Customer</a></li>
+		</ul>
+	</li>
+	<li><a href="#men">MEN</a></li>
+	<li><a href="#women">WOMEN</a></li>
+	<li><a href="#new releases">NEW_RELEASE</a></li>
+	<li><a href="#offers"> OFFERS</a></li>
+	<li><a href="#events">EVENTS</a></li>
+	</ul>
+	<form class="search-form">
+	<input type="text" placeholder ="Search">
+	<button> Search</button>  
+	</form>
+	</nav>
+        <p align="left" >We offer a quality service by supplying clothing and accessories at the most </br>competitive prices and the best quality in the market.
+		Legacy Stores Place,</br> have many years of experience in exporting and importing clothes and shoes, </br>working with the companies of collection 
+of clothing in Europe and UK, </br>so that our company has the best clothing and accessories in the market.</br>
+We export clothes to other African countries, Latin America and Asia, countries where we have regular customers who buy our products.</br>
+We also offer our products to retailers such as street vendors or second-hand stores, both national and international.</br>
+We are an international export company with experience and a professional approach for the</br> wholesale markets of clothes in Spain.
+We specialize in </br>high quality clothing and shoes, offering a high level of service to all our customers and you, </br>as a company,
+will get strong support in the wholesale markets.Here, at Legacy Stores Place, </br>our job is to provide premium quality clothes and shoes, 
+wholesale, to companies, both small and large.</br>We help beginners and promote professionals at higher levels. Our goal is that you become one of our clients,</br>
+being with us is a success for both of us. The company Legacy Stores Place, </br>offers a good product related to all market expectations covered by our business partners.</br>
+</p>
+   </body>
+</html>
